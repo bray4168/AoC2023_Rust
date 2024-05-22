a4aaca983710168bab3c839f0362c1698d153af8
Brandon <bray4168@yahoo.com>
2024-05-22 14:32:55 -0500

    First puzzle solution

---
+++ aoc_1/src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() {
+    let mut input: Vec<String> = vec![];
+    let mut values: Vec<u32> = vec![];
+    
+    read_file(&mut input).unwrap();
+    
+    for line in input {
+        get_values(&line, &mut values);
+    }
+
+    let sum: u32 = values.iter().sum();
+    println!("The answer is: {}", sum);
+}
+
+fn get_values(line: &String, values: &mut Vec<u32>) {
+    let mut array: [u32; 2] = [0, 0];
+    for character in line.chars() {
+        if character.is_numeric() && character.is_ascii() {
+            if array[0] == 0 {
+                array[0] = character.to_digit(10).unwrap();
+                array[1] = character.to_digit(10).unwrap();
+            }
+            else {
+                array[1] = character.to_digit(10).unwrap();
+            }
+        }
+    }
+    let value = array[0]*10 + array[1];
+    values.push(value);
+}
+
+
+fn read_file(input: &mut Vec<String>) -> io::Result<()> {
+    let f = File::open("input.txt")?;
+    let reader = BufReader::new(f);
+    
+    for line in reader.lines() {
+        input.push(line?);
+    }
+    Ok(())
+}
