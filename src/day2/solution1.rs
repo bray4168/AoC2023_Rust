6584c0e56b77f0d048873e21f6eb75d1b6bd9a58
Brandon <bray4168@yahoo.com>
2024-05-24 09:41:45 -0500

    Day2 solution 1 complete

--- src/day2/solution1.rs
+++ src/day2/solution1.rs
@@ -7,11 +7,45 @@ impl Solve for Solution1 {
     fn solve(&self) -> u32 {
         let mut input: Vec<String> = vec![];
         
-        input::read_file(&"src/day1/input.txt", &mut input).unwrap();
+        input::read_file(&"src/day2/input.txt", &mut input).unwrap();
         solution(input)
     }
 }
 
 pub fn solution(input: Vec<String>) -> u32 {
-    0
+    let mut values: Vec<u32> = vec![];
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
+
+    for each in input {
+        let split: Vec<&str> = each.split(":").collect::<Vec<_>>();
+        let mut id: u32 = split[0].split(" ").collect::<Vec<_>>()[1].parse().unwrap();
+        let pulls: Vec<&str> = split[1].split([';',',']).collect::<Vec<_>>();
+
+        for pull in pulls {
+            let pull_int: u32 = pull.split(" ").collect::<Vec<_>>()[1].parse().unwrap();
+            if pull.contains("green") {
+                if pull_int > max_green {
+                    id = 0;
+                }
+            }
+            else if pull.contains("red") {
+                if pull_int > max_red {
+                    id = 0;
+                }
+            }
+            else if pull.contains("blue") {
+                if pull_int > max_blue {
+                    id = 0;
+                }
+            }
+        }
+
+        values.push(id);
+    }
+
+    let sum: u32 = values.iter().sum();
+    println!("The solution for solution 1 is: {}", sum);
+    sum
 }
--- src/day2/test.rs
+++ src/day2/test.rs
@@ -1,19 +1,38 @@
 #[cfg(test)]
 mod tests {
-    use crate::input;
     use crate::solve::Solve;
     use crate::day2::Solution1;
     use crate::day2::Solution2;
+    use crate::day2::solution1;
+    use crate::day2::solution2;
 
     #[test]
     fn solution_1_test() {
         let solution = Solution1{};
-        assert_eq!(solution.solve(), 0);
+        let answer = 2377;
+        
+        assert_eq!(solution.solve(), answer);
+    }
+
+    #[test]
+    fn solution_1_test_basic() {
+        let input: Vec<String> = vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string(),
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue".to_string(),
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red".to_string(),
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red".to_string(),
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".to_string(),
+        ];
+        let answer = 8;
+        
+        assert_eq!(solution1::solution(input), answer);
     }
 
     #[test]
     fn solution_2_test() {
         let solution = Solution2{};
-        assert_eq!(solution.solve(), 0);
+        let answer = 0;
+        
+        assert_eq!(solution.solve(), answer);
     }
 }
