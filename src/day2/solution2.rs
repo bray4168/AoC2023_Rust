6854c019a52e87d625f3011e54508fb4235dbd34
Brandon <bray4168@yahoo.com>
2024-05-24 09:54:53 -0500

    Finish day2 part 2

--- src/day2/solution2.rs
+++ src/day2/solution2.rs
@@ -7,11 +7,43 @@ impl Solve for Solution2 {
     fn solve(&self) -> u32 {
         let mut input: Vec<String> = vec![];
         
-        input::read_file(&"src/day1/input.txt", &mut input).unwrap();
+        input::read_file(&"src/day2/input.txt", &mut input).unwrap();
         solution(input)
     }
 }
 
 pub fn solution(input: Vec<String>) -> u32 {
-    0
+    let mut values: Vec<u32> = vec![];
+
+    for each in input {
+        let mut min_red = 0;
+        let mut min_green = 0;
+        let mut min_blue = 0;
+        let split: Vec<&str> = each.split(":").collect::<Vec<_>>();
+        let pulls: Vec<&str> = split[1].split([';',',']).collect::<Vec<_>>();
+
+        for pull in pulls {
+            let pull_int: u32 = pull.split(" ").collect::<Vec<_>>()[1].parse().unwrap();
+            if pull.contains("green") {
+                if pull_int > min_green {
+                    min_green = pull_int;
+                }
+            }
+            else if pull.contains("red") {
+                if pull_int > min_red {
+                    min_red = pull_int;
+                }
+            }
+            else if pull.contains("blue") {
+                if pull_int > min_blue {
+                    min_blue = pull_int;
+                }
+            }
+        }
+        values.push(min_green * min_red * min_blue);
+    }
+
+    let sum: u32 = values.iter().sum();
+    println!("The solution for solution 2 is: {}", sum);
+    sum
 }
--- src/day2/test.rs
+++ src/day2/test.rs
@@ -31,8 +31,22 @@ mod tests {
     #[test]
     fn solution_2_test() {
         let solution = Solution2{};
-        let answer = 0;
+        let answer = 71220;
         
         assert_eq!(solution.solve(), answer);
+    }
+
+    #[test]
+    fn solution_2_test_basic() {
+        let input: Vec<String> = vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string(),
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue".to_string(),
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red".to_string(),
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red".to_string(),
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".to_string(),
+        ];
+        let answer = 2286;
+        
+        assert_eq!(solution2::solution(input), answer);
     }
 }
